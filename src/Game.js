0cb2a49c9cfc80da9a5ad5598aecade986857206
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-10-03 15:14:14 -0600

    Write game.js doc with tests for other docs

--- src/Game.js
+++ src/Game.js
@@ -3,8 +3,8 @@ const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Game {
-  constructor() {
-
+  constructor(round) {
+    this.currentRound = round;
   }
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -13,6 +13,18 @@ class Game {
   printQuestion(round) {
       util.main(round);
   }
+  start() {
+    const cards = [];
+    prototypeData.forEach() {
+      const card = new Card(prototypeQuestions.id, prototypeQuestions.question, prototypeQuestions.answers, prototypeQuestions.correctAnswer);
+      cards.push(card);
+    }
+    const deck = new Deck(cards);
+    const round = new Round(deck);
+    const game = new Game (round);
+    this.printMessage(deck, round);
+    this.printQuestion(round);
+  };
 }
 
 module.exports = Game;
--- test/Game-test.js
+++ test/Game-test.js
@@ -2,7 +2,33 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Game = require('../src/Game');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+
 
 describe('Game', function() {
+  it('should have a property of current round', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    const game = new Game(round);
+    expect(game.currentRound).to.equal(round);
+  });
 
+  it('should have a start function', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    const game = new Game(round);
+    expect(game.start).to.be.a('function');
+  });
+
+  it('should have a set of cards', function() {
+    expect(game.start()).to.deep.equal()
+  })
 });
